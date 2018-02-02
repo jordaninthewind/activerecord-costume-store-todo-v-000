6fa2279289e16df3268aa739c7c9901d63792350
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 20:26:58 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,13 +2,13 @@
 class HauntedHouse < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |el|
-      el.text :name,
-      el.text :location,
-      el.text :theme,
-      el.float :price,
-      el.boolean :family_friendly,
-      el.datetime :opening_date,
-      el.datetime :closing_date,
+      el.text :name
+      el.text :location
+      el.text :theme
+      el.float :price
+      el.boolean :family_friendly
+      el.datetime :opening_date
+      el.datetime :closing_date
       el.text :description
     end
   end
