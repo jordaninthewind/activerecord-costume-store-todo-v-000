8db6cf82bb2e3afe4a1282586bd2d1e80e1cf5b7
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 20:23:28 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,5 +5,7 @@ class HauntedHouse < ActiveRecord::Migration[4.2]
     el.text :location,
     el.text :theme,
     el.float :price,
-    
+    el.boolean :family_friendly,
+    el.datetime :opening_date,
+    el.datetime :closing_date,
 end
