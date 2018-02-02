aa3d7e1f49d2e84479a2ba826ac3a081c7a6880e
Jordan Kline <jordan.kline@gmail.com>
2018-02-02 20:22:45 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,9 @@
 # Create your haunted_houses migration here
 class HauntedHouse < ActiveRecord::Migration[4.2]
-
+  create_table :haunted_houses do |el|
+    el.text :name,
+    el.text :location,
+    el.text :theme,
+    el.float :price,
+    
 end
